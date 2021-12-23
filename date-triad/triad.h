cf41000d684f1e837329b422048c4290401917c6
Gleb <perveev@surfstudio.ru>
2021-12-23 21:13:13 +0300

    feat: Добавлены основные методы и конструкторы класса Triad

--- date-triad/triad.h
+++ date-triad/triad.h
@@ -1,8 +1,30 @@
 #ifndef TRIAD_H
 #define TRIAD_H
+#include "header.h"
 
 class Triad
 {
-};
+protected:
+	int first_;
+	int second_;
+	int third_;
 
+public:
+	Triad();
+	Triad(const int& first, const int& second, const int& third);
+	Triad(const triple_t& triple);
+
+
+	int getFirst() const;
+	int getSecond() const;
+	int getThird() const;
+	triple_t getTriple() const;
+
+	void setFirst(const int& first);
+	void setSecond(const int& second);
+	void setThird(const int& third);
+	void setTriple(const triple_t& triple);
+
+	string toString() const;
+};
 #endif
