8abf71abe3dc3ff73ade036dd4247573c533492a
Gleb <perveev@surfstudio.ru>
2021-12-23 21:54:17 +0300

    feat: Добавлены исключения и .т.п

--- date-triad/header.h
+++ date-triad/header.h
@@ -6,8 +6,13 @@
 using std::string;
 using std::to_string;
 using std::get;
+using std::tuple;
+using std::isdigit;
+using std::exception;
+using std::stoul;
+using std::abs;
 
-using triple_t = std::tuple<int, int, int>;
+using triple_t = tuple<size_t, size_t, size_t>;
 
 
 #endif
